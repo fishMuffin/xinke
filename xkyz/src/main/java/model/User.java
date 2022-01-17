1fdb303ec5629f6934c7941c85f8d097eb8188d6
Klein <yinchengwen@magiccreative.tech>
2022-01-17 14:57:09 +0800

    add swagger config and define the user model class

---
+++ xkyz/src/main/java/model/User.java
@@ -0,0 +1,36 @@
+package model;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+@ApiModel("用户实体类")
+public class User {
+    @ApiModelProperty("id")
+    private Integer id;
+    @ApiModelProperty("用户名")
+    public String username;
+    @ApiModelProperty("密码")
+    public String password;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
